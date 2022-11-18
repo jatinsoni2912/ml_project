3030d380de53b8e6e4151fcb861745f74b70b9a6
jatinsoni2912 <jatinsoni2912@gmail.com>
2022-11-18 16:19:25 +0000

    changed files experiment_builder and train_evaluate

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -154,10 +154,9 @@ class ExperimentBuilder(nn.Module):
         Complete the code in the block below to collect absolute mean of the gradients for each layer in all_grads with the             layer names in layers.
         """
         ########################################
-        for n, p in named_parameters:
-            if(p.requires_grad) and (bias not in n):
-                layers.append(n)
-                all_grads.append(p.grad.abs().mean())
+        for layer, weights in named_parameters:
+            layers.append(layer)
+            all_grads.append(torch.mean(torch.abs(weights.grad)))
         ########################################
             
         
